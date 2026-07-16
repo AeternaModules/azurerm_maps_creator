01ebf73149af93d9b500e70145b49c9db0381847
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "maps_creators_id" {
   description = "Map of id values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.id }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "maps_creators_location" {
   description = "Map of location values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.location }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "maps_creators_maps_account_id" {
   description = "Map of maps_account_id values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.maps_account_id }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.maps_account_id if v.maps_account_id != null && length(v.maps_account_id) > 0 }
 }
 output "maps_creators_name" {
   description = "Map of name values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.name }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "maps_creators_storage_units" {
   description = "Map of storage_units values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.storage_units }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.storage_units if v.storage_units != null }
 }
 output "maps_creators_tags" {
   description = "Map of tags values across all maps_creators, keyed the same as var.maps_creators"
-  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.tags }
+  value       = { for k, v in azurerm_maps_creator.maps_creators : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
